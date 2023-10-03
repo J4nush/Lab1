f587f0dfc4a9cc2007d6ecd4ffaf8869f90472be
J4nush <michal.ziolek@pollub.edu.pl>
2023-10-03 17:00:13 +0200

    tets

---
+++ Lab1/MyView.swift
@@ -0,0 +1,18 @@
+//
+//  MyView.swift
+//  Lab1
+//
+//  Created by student on 03/10/2023.
+//
+
+import SwiftUI
+
+struct MyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyView()
+}
